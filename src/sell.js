cee8671db8bc3ccb0eebcaade0adb6136c9d8ce1
Tim Horton <tmhorton@gmail.com>
2019-02-03 21:56:25 -0800

    Update API to match simplex docs

--- src/sell.js
+++ src/sell.js
@@ -1,4 +1,5 @@
 const rp = require('request-promise');
+const models = require('./models')
 
 const REFERER_URL = "https://edge.app"
 
@@ -12,6 +13,7 @@ module.exports = function (sandbox, partnerId, apiKey) {
 
   const API_BASE = sandbox
     // ? 'https://sell-sandbox.test-simplexcc.com/v1'
+    // ? 'https://api.test-simplexcc.com/v1'
     ? 'https://api.test-simplexcc.com/v1'
     : 'https://api.simplexcc.com/v1';
 
@@ -41,7 +43,7 @@ module.exports = function (sandbox, partnerId, apiKey) {
     return rp(options)
   }
 
-  function initiateSell(req, clientIp) {
+  async function initiateSell(req, clientIp) {
     const data = {
       referer_url: REFERER_URL,
       return_url: req.body.return_url,
@@ -60,22 +62,30 @@ module.exports = function (sandbox, partnerId, apiKey) {
       body: data,
       json: true
     }
-    console.log(options)
-    return rp(options)
+    const res = await rp(options)
+    const sellRequest = await models.SellRequest.create({
+      quote_id: data.txn_details.quote_id,
+      account_id: data.account_details.account_id,
+      txn_id: res.txn_id,
+      txn_url: res.txn_url
+    })
+    return res
   }
 
   async function userQueue(req) {
-    let data = await messages()
-    return data.messages
+    let data = await models.SendCrypto({
+      where: { account_id: req.params.userid }
+    })
+    return data
   }
 
-  async function notifyUser(req) {
+  async function notifyUser(txnId) {
     const options = {
       uri: `${API_BASE}/notify-user`,
       method: 'POST',
       headers: HEADERS,
       data: {
-        txn_id: req.params.txn_id,
+        txn_id: txnId,
         template_name: 'execution-order-deeplink',
         template_params: {
           deeplink: 'edge://plugins/simplex'
@@ -87,14 +97,40 @@ module.exports = function (sandbox, partnerId, apiKey) {
     return rp(options)
   }
 
-  async function messages() {
+  async function messageAck(msgId) {
+    const options = {
+      uri: `${API_BASE}/msg/${msgId}/ack`,
+      method: 'POST',
+      headers: HEADERS,
+      json: true
+    }
+    return rp(options)
+  }
+
+  async function syncMessages() {
     const options = {
       uri: `${API_BASE}/msg`,
       method: 'GET',
       headers: HEADERS,
       json: true
     }
-    return rp(options)
+    const res = await rp(options)
+    console.log(JSON.stringify(res))
+    for (let i = 0; i < res.length; ++i) {
+      const m = res[i].msg
+      const c = await models.SendCrypto.create({
+        reason: m.reason,
+        txn_id: m.txn_id,
+        user_id: m.user_id,
+        account_id: m.account_id,
+        quote_id: m.account_id,
+        crypto_currency: m.crypto_currency,
+        crypto_amount_sent: m.crypto_amount,
+        destination_crypto_address: m.destination_crypto_address
+      })
+      await messageAck(m.id)
+      await notifyUser(m.txn_id)
+    }
   }
 
   async function messageResponse(req) {
@@ -116,19 +152,9 @@ module.exports = function (sandbox, partnerId, apiKey) {
     return rp(options)
   }
 
-  async function messageAck(req) {
-    const options = {
-      uri: `${API_BASE}/msg/${req.params.msg_id}/ack`,
-      method: 'POST',
-      headers: HEADERS,
-      json: true
-    }
-    return rp(options)
-  }
-
   return {
     getQuote, initiateSell,
-    messages, messageAck, messageResponse,
-    userQueue, notifyUser
+    syncMessages, messageAck, messageResponse,
+    userQueue
   }
 }
